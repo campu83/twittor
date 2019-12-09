52cb490c873b47fe02d7b4d73a9cb6e47800b12f
Antonio Esteban Campuzano Guillén <antonioecampuzano@hotmail.com>
2019-12-09 09:04:02 +0100

    modificaciones para ios

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const PATH_BASE ='';
 //imports
 importScripts(PATH_BASE+'js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v3';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
